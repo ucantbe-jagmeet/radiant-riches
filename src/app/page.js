15ed3d936e6e563461421232f8bb95cf7e1a2baf
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-02-26 23:06:34 +0530

    data added to main

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero";
+import Main from "@/components/Main";
 
 export default function Home() {
   return (
     <main className="w-full flex justify-center ">
       <div className="w-[1000px]">
         <Hero />
+        <Main />
       </div>
     </main>
   );
---
+++ src/components/Main.js
@@ -0,0 +1,78 @@
+import React from 'react'
+
+const data = [
+    {
+        bestChoice: true,
+        bestValue: false,
+        discount: 0,
+        name: 'Builder 1',
+        title: "WixPro 72-Inch Responsive Website Builder",
+        text: "Comprehensive Digital Platform Creation Tool, Streamlined Design Interface for Professional Websites and Online Stores (Black/Blue)",
+        highlights: "[What You Get]: Receive the WixPro website builder suite, access to premium design templates, an extensive library of widgets and apps, and detailed step-by-step guides.",
+        rating: 9.8,
+        stars: 5,
+        ratingComment: "Exceptional",
+    },
+    {
+        bestChoice: false,
+        bestValue: true,
+        discount: 0,
+        name: 'Builder',
+        title: "SiteCraft 68-Inch Ultimate Web Design Studio",
+        text: "Advanced Site Creation Toolkit, Intuitive Drag-and-Drop Editor for Dynamic Websites and E-commerce Platforms (Green/White)",
+        highlights: "[What You Get]: Gain access to the SiteCraft design studio, featuring a robust selection of design elements, SEO optimization tools, and e-commerce integrations.",
+        rating: 9.5,
+        stars: 4.5,
+        ratingComment: "Excellent",
+    },
+    {
+        bestChoice: false,
+        bestValue: false,
+        discount: 0,
+        name: 'Builder 1',
+        title: "WixPro 72-Inch Responsive Website Builder",
+        text: "An extensive library of widgets and apps, and detailed step-by-step guides)",
+        highlights: "[What You Get]: Receive the WixPro website builder suite, access to premium design templates, an extensive library of widgets and apps, and detailed step-by-step guides.",
+        rating: 9.3,
+        stars: 5,
+        ratingComment: "Exceptional",
+    },
+    {
+        bestChoice: false,
+        bestValue: false,
+        name: 'CDK',
+        discount: 26,
+        title: "CDK Resposive Builder",
+        text: "Comprehensive Digital Platform Creation Tool, Streamlined Design Interface for Professional Websites and Online Stores (Black/Blue)",
+        highlights: "",
+        highlightsMenu: [
+            {
+                value: 9.9,
+                text: "Building Responsive"
+            },
+            {
+                value: 8.9,
+                text: "Cool"
+            },
+            {
+                value: 8.9,
+                text: "Docs"
+            }
+
+        ],
+        features: ["Documentatio", "Easy Use", "Out of Box"],
+        rating: 9.8,
+        stars: 5,
+        ratingComment: "Exceptional",
+    },
+]
+
+const Main = () => {
+    return (
+        <div>
+            main
+        </div>
+    )
+}
+``
+export default Main
